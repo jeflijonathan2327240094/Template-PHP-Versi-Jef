026d54326762c904460262cb21817f0e101ebe9d
jeflijonathan2327240094 <156399021+jeflijonathan2327240094@users.noreply.github.com>
2025-02-20 09:40:34 +0700

    change: name file character

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,6 @@
 <?php
-// include_once '../Main.php';
-// include_once '../Connection';
-
 class Home {
     public function showPage(): string {
-       
         $content = <<<HTML
             <h1>
                 Hello. Welcome to My Website
